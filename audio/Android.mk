e59feb95b84c9ec4987ad6a9251c8823677677b2
Hashcode <hashcode0f@gmail.com>
2013-04-05 13:47:47 -0700

    audio: remove custom audio_policy -- didnt need it
    Change-Id: I6c6d11d5b2a32a0529248fc2c9e57dca9b7fb35c

--- audio/Android.mk
+++ audio/Android.mk
@@ -44,29 +44,4 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
-###
-### SLIGHTLY CUSTOM AUDIO_POLICY WITH VOLUME HOOKS FOR AUDIO PRIMARY
-###
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    AudioPolicyManagerDefault.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libcutils \
-    libutils
-
-LOCAL_STATIC_LIBRARIES := \
-    libmedia_helper
-
-LOCAL_WHOLE_STATIC_LIBRARIES := \
-    libaudiopolicy_legacy
-
-LOCAL_MODULE := audio_policy.$(TARGET_BOOTLOADER_BOARD_NAME)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
 endif
