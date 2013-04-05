b7c132781f2ab6651d9ae889464be394ff193b7e
Hashcode <hashcode0f@gmail.com>
2013-04-05 09:29:45 -0700

    WIP: customize audio_policy
    Change-Id: Idb845837678e90a7b9c4ef904c82a12bc92b6ed7

--- audio/Android.mk
+++ audio/Android.mk
@@ -2,6 +2,10 @@ ifeq ($(findstring bowser, $(TARGET_BOOTLOADER_BOARD_NAME)),bowser)
 
 LOCAL_PATH := $(call my-dir)
 
+###
+### AUDIO PRIMARY FOR BOWSER
+###
+
 include $(CLEAR_VARS)
 
 # Should change this so the enable variable gets used as the name?
@@ -36,6 +40,31 @@ LOCAL_C_INCLUDES += \
 	system/media/audio_utils/include \
 	system/media/audio_effects/include
 LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+###
+### SLIGHTLY CUSTOM AUDIO_POLICY WITH VOLUME HOOKS FOR AUDIO PRIMARY
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    AudioPolicyManagerDefault.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libutils
+
+LOCAL_STATIC_LIBRARIES := \
+    libmedia_helper
+
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+    libaudiopolicy_legacy
+
+LOCAL_MODULE := audio_policy.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
