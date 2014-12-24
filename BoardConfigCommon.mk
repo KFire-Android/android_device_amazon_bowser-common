e31a6a4ffc3ab18f117906d3d985f6561840b5a5
Hashcode <hashcode0f@gmail.com>
2014-12-24 10:42:23 -0800

    bowser-common: clean up removed releasetool override
    Change-Id: I5d750b4bf805ee10a71aef718fae2fab4b023ede

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,5 +68,4 @@ LEGACY_RIL := true
 
 # hack the ota
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./$(COMMON_FOLDER)/releasetools/bowser_ota_from_target_files.py
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./$(COMMON_FOLDER)/releasetools/bowser_img_from_target_files.py
 TARGET_RELEASETOOL_MAKE_RECOVERY_PATCH_SCRIPT := ./$(COMMON_FOLDER)/releasetools/bowser_make_recovery_patch
